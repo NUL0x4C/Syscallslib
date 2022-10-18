0da2b8f6240583d4612126dc33752283ee74ba1a
ORCA <ORCx41@gmail.com>
2022-10-18 08:49:15 -0700

    Update Test.c

--- Test/Test.c
+++ Test/Test.c
@@ -6,31 +6,39 @@
 
 
 // generated using hasher.c (Seed = 7)
-#define NtAllocateVirtualMemory_StrHashed       0x014044AE
-#define NtProtectVirtualMemory_StrHashed        0xE67C7320
-#define NtCreateSection_StrHashed       0xAC2EDA02
-#define NtOpenSection_StrHashed         0xD443EC8C
-#define NtMapViewOfSection_StrHashed    0x92DD00B3
-#define NtUnmapViewOfSection_StrHashed  0x12D71086
-#define NtClose_StrHashed       0x7B3F64A4
+#define NtAllocateVirtualMemory_StrHashed   0x014044AE
+#define NtProtectVirtualMemory_StrHashed    0xE67C7320
+#define NtCreateSection_StrHashed			0xAC2EDA02
+#define NtOpenSection_StrHashed				0xD443EC8C
+#define NtMapViewOfSection_StrHashed		0x92DD00B3
+#define NtUnmapViewOfSection_StrHashed		0x12D71086
+#define NtClose_StrHashed					0x7B3F64A4
+#define NtQuerySystemInformation_StrHashed  0xEFFC1CF8
+#define NtCreateUserProcess_StrHashed		0x0C43BACB
+#define NtWriteVirtualMemory_StrHashed		0x1130814D
+
+
+
 
 
 int main() {
 
 	NTSTATUS	STATUS		= NULL;
-	PVOID		pAddress	= NULL;
 	
 
 
 	HashStruct SyscallHashStruct = {
 
-	.NtAllocateVirtualMemory_Hash = NtAllocateVirtualMemory_StrHashed,
+	.NtAllocateVirtualMemory_Hash	= NtAllocateVirtualMemory_StrHashed,
 	.NtProtectVirtualMemory_Hash	= NtProtectVirtualMemory_StrHashed,
 	.NtCreateSection_Hash			= NtCreateSection_StrHashed,
-	.NtOpenSection_Hash			= NtOpenSection_StrHashed,
+	.NtOpenSection_Hash				= NtOpenSection_StrHashed,
 	.NtMapViewOfSection_Hash		= NtMapViewOfSection_StrHashed,
-	.NtUnmapViewOfSection_Hash	= NtUnmapViewOfSection_StrHashed,
+	.NtUnmapViewOfSection_Hash		= NtUnmapViewOfSection_StrHashed,
 	.NtClose_Hash					= NtClose_StrHashed,
+	.NtWriteVirtualMemory_Hash		= NtWriteVirtualMemory_StrHashed,
+	.NtQuerySystemInformation_Hash	= NtQuerySystemInformation_StrHashed,
+	.NtCreateUserProcess_Hash		= NtCreateUserProcess_StrHashed
 
 	};
 
@@ -41,17 +49,11 @@ int main() {
 	}
 
 
-	pAddress = NtAllocateVirtualMemory2(0x100, &STATUS);
-	if (pAddress == NULL) {
-		printf("[-] Error Occured : 0x%0.8X \n", STATUS);
-		goto _exit;
-	}
-	printf("[+] pAddress : 0x%p \n", pAddress);
-
-
+	
 
 
 _exit:
+	
 	printf("[i] Press <Enter> To Quit ... ");
 	getchar();
 	return 0;
